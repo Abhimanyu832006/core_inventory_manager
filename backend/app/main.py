c7c752aceb6f53c0fd7eb2b792933a3366f702af
Abhimanyu Singh Rajawat <abhimanyurajawat08@gmail.com>
2026-03-14 09:56:44 +0000

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,7 +25,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Lock this down in production
+    allow_origins=["http://localhost:3000",
+        "http://localhost:57982",
+        "http://127.0.0.1:3000",
+        "http://192.168.1.9:8000",],  # Lock this down in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
